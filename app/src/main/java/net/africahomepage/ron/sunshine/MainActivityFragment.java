5287c745a580040f90dba3f00f2c79c2cdbe726b
ron <ronald.marangwanda@gmail.com>
2015-06-27 12:10:02 -0400

    syntax changes

--- app/src/main/java/net/africahomepage/ron/sunshine/MainActivityFragment.java
+++ app/src/main/java/net/africahomepage/ron/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -12,12 +18,30 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    public ArrayAdapter<String> adapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main,container);
+        String[] data = new String[]{
+                "Monday-Sunny-23/18",
+                "Monday-Sunny-23/18",
+                "Monday-Sunny-23/18",
+                "Monday-Sunny-23/18",
+                "Monday-Sunny-23/18"
+        };
+
+        List<String> dataArray = new ArrayList<>(
+                Arrays.asList(data)
+        );
+
+        adapter = new ArrayAdapter<>(getActivity(),R.layout.list_item_forecast, R.id.list_item_forecast_textview, dataArray);
+        ListView list = (ListView)root.findViewById(R.id.listview_forecast);
+        list.setAdapter(adapter);
+        return root;
     }
 }
